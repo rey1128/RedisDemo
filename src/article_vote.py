7aa598f56091c080c7f549f326e28df9470000c9
yutong <yu.tong@widas.de>
2019-05-17 15:27:38 +0200

    format service

--- src/article_vote.py
+++ src/article_vote.py
@@ -2,33 +2,33 @@ import article_service
 import common_constants
 
 
-def vote_up(redis_conn, user, article):
-    if article_service.is_article_expired(redis_conn, article):
-        print(article + ' is expired')
+def vote_up(redis_conn, user, article_key):
+    if article_service.is_article_expired(redis_conn, article_key):
+        print(article_key + ' is expired')
         return False
-    article_id = article.partition(':')[-1]
+    article_id = article_key.partition(':')[-1]
     if redis_conn.smove('voted:down:' + article_id, 'voted:up:' + article_id, user) or redis_conn.sadd(
             'voted:up:' + article_id,
             user):
-        redis_conn.zincrby('score:', common_constants.VOTE_SCORE, article)
-        redis_conn.hincrby(article, 'votes', 1)
+        redis_conn.zincrby('score:', common_constants.VOTE_SCORE, article_key)
+        redis_conn.hincrby(article_key, 'votes', 1)
         return True
     else:
-        print(user + ' already votes up on ' + article)
+        print(user + ' already votes up on ' + article_key)
     return False
 
 
-def vote_down(redis_conn, user, article):
-    if article_service.is_article_expired(redis_conn, article):
-        print(article + ' is expired')
+def vote_down(redis_conn, user, article_key):
+    if article_service.is_article_expired(redis_conn, article_key):
+        print(article_key + ' is expired')
         return False
-    article_id = article.partition(':')[-1]
+    article_id = article_key.partition(':')[-1]
     if redis_conn.smove('voted:up' + article_id, 'voted:down:' + article_id, user) or redis_conn.sadd(
             'voted:down:' + article_id,
             user):
-        redis_conn.zincrby('score:', -common_constants.VOTE_SCORE, article)
-        redis_conn.hincrby(article, 'votes', -1)
+        redis_conn.zincrby('score:', -common_constants.VOTE_SCORE, article_key)
+        redis_conn.hincrby(article_key, 'votes', -1)
         return True
     else:
-        print(user + ' already votes down on ' + article)
+        print(user + ' already votes down on ' + article_key)
     return False
